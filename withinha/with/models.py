1400ae8082317d669bc625a9319170aa80a428da
YeomJongSeop <bhs3435@naver.com>
2024-08-04 20:38:33 +0900

    Delete Allauth email

--- withinha/with/models.py
+++ withinha/with/models.py
@@ -11,7 +11,7 @@ class MyUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+    
     def create_superuser(self, id, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
